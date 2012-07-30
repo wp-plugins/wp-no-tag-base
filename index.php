676ee271caabedff016830b22820b15c771e667f
dlocc <dlocc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-30 20:02:31 +0000

    Patch release
    git-svn-id: https://plugins.svn.wordpress.org/wp-no-tag-base/tags/v1.2.2@579454 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    // Show plugin info instead of directory listing
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<title>WP No Tag Base</title>
+	</head>
+	<body>
+		<ul>
+			<li><strong>WP No Tag Base</strong> plugin</li>
+			<li><a href="http://wordpresssupplies.com/wordpress-plugins/no-tag-base/">Plugin Homepage</a></li>
+			<li><a href="http://wordpress.org/extend/plugins/wp-no-category-base/">Download Plugin</a></li>
+			<li><a href="http://wordimpressed.com/">Author Homepage</a></li>
+			<li><a href="http://wordimpressed.com/">Donate to this plugin</a></li>
+		</ul>
+	</body>
+</html>
